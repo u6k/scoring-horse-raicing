b60b03a2fb71a4c739be1c3eade6ac72c572c85d
u6k <u6k.apps@gmail.com>
2018-07-17 21:14:13 +0900

    impl race_list_page download

--- app/models/race_list_page.rb
+++ app/models/race_list_page.rb
@@ -1,2 +1,26 @@
 class RaceListPage < ApplicationRecord
+
+  validates :date, presence: true, uniqueness: true
+
+  attr_accessor :content
+
+  def self.download(year, month, day)
+    date = Time.zone.local(year, month, day, 0, 0, 0)
+
+    url = "https://www.oddspark.com/keiba/KaisaiRaceList.do?raceDy=#{date.strftime('yyyyMMdd')}"
+
+    content = NetModule.download_with_get(url)
+
+    _initialize(date, content)
+  end
+
+  private
+
+  def self._initialize(date, content)
+    race_list_page = RaceListPage.new(date: date)
+    race_list_page.content = content
+
+    race_list_page
+  end
+
 end
--- test/models/race_list_page_test.rb
+++ test/models/race_list_page_test.rb
@@ -3,19 +3,17 @@ require 'test_helper'
 class RaceListPageTest < ActiveSupport::TestCase
 
   test "download race list page" do
+    # precondition
+    year = 2018
+    month = 7
+    day = 16
+
     # execute
-    race_list_page = RaceListPage.download(2018, 7, 16)
+    race_list_page = RaceListPage.download(year, month, day)
 
     # postcondition
-    assert_equal Time.zone.local(2018, 7, 16, 0, 0, 0), race_list_page.date
+    assert_equal Time.zone.local(year, month, day, 0, 0, 0), race_list_page.date
     assert race_list_page.content.length > 0
-  end
-
-  test "download race list page: case invalid date" do
-    # execute
-    assert_raise RuntimeError, "race_list_page not found. date:2018-02-31" do
-      RaceListPage.download(2018, 2, 31)
-    end
   end
 
 end

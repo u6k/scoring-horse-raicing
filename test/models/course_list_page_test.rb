2ddfd9c7cc161bfdf1fe791af86fde7a91267b40
u6k <u6k.apps@gmail.com>
2018-07-22 02:25:03 +0900

    impl test

--- test/models/course_list_page_test.rb
+++ test/models/course_list_page_test.rb
@@ -163,6 +163,60 @@ class CourseListPageTest < ActiveSupport::TestCase
   end
 
   test "parse race page" do
+    # precondition
+    html3 = File.open("test/fixtures/files/course_list/course_list.20180716.html").read
+    NetModule.put_s3_object(NetModule.get_s3_bucket, "course_list/course_list.20180716.html", html3)
+    course_list_page = CourseListPage.new(date: Time.zone.local(2018, 7, 16, 0, 0, 0), url: "http://example.com/course_list.20180716.html")
+
+    # execute
+    race_list_pages = course_list_page.download_race_list_pages
+
+    # postcondition
+    assert_equal 5, race_list_pages.length
+
+    p race_list_pages.inspect # TODO
+
+    race_list_page = race_list_pages[0]
+    assert_equal "帯広競馬場", race_list_page.course_name
+    assert_equal "ナイター", race_list_page.timezone
+    assert race_list_page.content.length > 0
+    assert race_list_page.course_list_page.same?(course_list_page)
+    assert race_list_page.valid?
+    assert_not @bucket.object("race_list/20180716/race_list.帯広競馬場.html").exists?
+
+    race_list_page = race_list_pages[1]
+    assert_equal "盛岡競馬場", race_list_page.course_name
+    assert_equal "薄暮", race_list_page.timezone
+    assert race_list_page.content.length > 0
+    assert race_list_page.course_list_page.same?(course_list_page)
+    assert race_list_page.valid?
+    assert_not @bucket.object("race_list/20180716/race_list.盛岡競馬場.html").exists?
+
+    race_list_page = race_list_pages[2]
+    assert_equal "金沢競馬場", race_list_page.course_name
+    assert_equal "薄暮", race_list_page.timezone
+    assert race_list_page.content.length > 0
+    assert race_list_page.course_list_page.same?(course_list_page)
+    assert race_list_page.valid?
+    assert_not @bucket.object("race_list/20180716/race_list.金沢競馬場.html").exists?
+
+    race_list_page = race_list_pages[3]
+    assert_equal "高知競馬場", race_list_page.course_name
+    assert_equal "ナイター", race_list_page.timezone
+    assert race_list_page.content.length > 0
+    assert race_list_page.course_list_page.same?(course_list_page)
+    assert race_list_page.valid?
+    assert_not @bucket.object("race_list/20180716/race_list.高知競馬場.html").exists?
+
+    race_list_page = race_list_pages[4]
+    assert_equal "佐賀競馬場", race_list_page.course_name
+    assert_equal "薄暮", race_list_page.timezone
+    assert race_list_page.content.length > 0
+    assert race_list_page.course_list_page.same?(course_list_page)
+    assert race_list_page.valid?
+    assert_not @bucket.object("race_list/20180716/race_list.佐賀競馬場.html").exists?
+
+    assert_equal 0, RaceListPage.all.length
   end
 
 end

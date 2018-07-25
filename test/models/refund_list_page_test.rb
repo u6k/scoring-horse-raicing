2f09c8b234f3f3748c9dae7494ce655c45781c34
u6k <u6k.apps@gmail.com>
2018-07-25 17:21:15 +0900

    modify new instance at test

--- test/models/refund_list_page_test.rb
+++ test/models/refund_list_page_test.rb
@@ -12,16 +12,16 @@ class RefundListPageTest < ActiveSupport::TestCase
     course_list_page = CourseListPage.download(2018, 7, 16)
     course_list_page.save!
 
-    race_list_pages = course_list_page.download_race_list_pages
-    race_list_pages.each { |r| r.save! }
+    race_list_page = RaceListPage.download(course_list_page, "帯広競馬場", "ナイター", "https://www.oddspark.com/keiba/OneDayRaceList.do?raceDy=20180716&opTrackCd=03&sponsorCd=04")
+    race_list_page.save!
 
     # execute 1
-    refund_list_page = race_list_pages[0].download_refund_list_page
+    refund_list_page = race_list_page.download_refund_list_page
 
     # postcondition
     assert_equal "https://www.oddspark.com/keiba/RaceRefund.do?sponsorCd=04&raceDy=20180716&opTrackCd=03", refund_list_page.url
     assert refund_list_page.content.length > 0
-    assert refund_list_page.race_list_page.same?(race_list_pages[0])
+    assert refund_list_page.race_list_page.same?(race_list_page)
     assert refund_list_page.valid?
     assert_not @bucket.object("race_list/20180716/帯広競馬場/refund_list.html").exists?
 
@@ -33,7 +33,7 @@ class RefundListPageTest < ActiveSupport::TestCase
     # postcondition 2
     assert_equal 1, RefundListPage.all.length
 
-    assert refund_list_page.same?(race_list_pages[0].refund_list_page)
+    assert refund_list_page.same?(race_list_page.refund_list_page)
 
     assert @bucket.object("race_list/20180716/帯広競馬場/refund_list.html").exists?
   end

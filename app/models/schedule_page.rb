0072b7eb93bc845602495b004c09d1481f309b07
u6k <u6k.apps@gmail.com>
2018-07-31 11:37:21 +0900

    解析を実装した

--- app/models/schedule_page.rb
+++ app/models/schedule_page.rb
@@ -5,11 +5,13 @@ class SchedulePage < ApplicationRecord
 
   attr_accessor :content
 
-  def self.download(year, month)
+  def self.download(year, month, content = nil)
     datetime = Time.zone.local(year, month, 1, 0, 0, 0)
     url = "https://keiba.yahoo.co.jp/schedule/list/#{datetime.year}/?month=#{datetime.month}"
 
-    content = NetModule.download_with_get(url)
+    if content.nil?
+      content = NetModule.download_with_get(url)
+    end
 
     schedule_page = find_by_url(url)
     if schedule_page.nil?
@@ -20,6 +22,33 @@ class SchedulePage < ApplicationRecord
     schedule_page.content = content
 
     schedule_page
+  end
+
+  def parse
+    doc = Nokogiri::HTML.parse(@content, nil, "UTF-8")
+
+    page_data = doc.xpath("//table[contains(@class, 'scheLs')]/tbody/tr/td[position()=1 and @rowspan='2']").map do |td|
+      course_info = {}
+
+      td.text.match(/([0-9]+)日/) do |day|
+        course_info[:date] = Time.zone.local(self.datetime.year, \
+          self.datetime.month, \
+          day[1].to_i, \
+          0, 0, 0)
+      end
+
+      if not td.xpath("a").nil?
+        td.xpath("a").attribute("href").value.match(/\/race\/list\/[0-9]+\//) do |path|
+          course_info[:url] = "https://keiba.yahoo.co.jp" + path[0]
+        end
+
+        td.xpath("a").text.match(/^[0-9]+回(.+?)[0-9]+日/) do |course|
+          course_info[:course_name] = course[1]
+        end
+      end
+
+      course_info
+    end
   end
 
 end

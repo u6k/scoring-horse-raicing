a9454e918835fde634bcbfe2707dc46d16d94da4
u6k <u6k.apps@gmail.com>
2018-07-31 17:46:11 +0900

    ダウンロード処理を実装した

--- app/models/race_list_page.rb
+++ app/models/race_list_page.rb
@@ -1,3 +1,25 @@
 class RaceListPage < ApplicationRecord
+
+  validates :url, presence: true, uniqueness: true
+  validates :date, presence: true
+  validates :course_name, presence: true
+
+  attr_accessor :content
   belongs_to :schedule_page
+
+  def self.download(schedule_page, url, date, course_name)
+    content = NetModule.download_with_get(url)
+
+    race_list_page = find_by_url(url)
+    if race_list_page.nil?
+      race_list_page = schedule_page.race_list_pages.build(url: url, date: date, course_name: course_name)
+    else
+      race_list_page.date = date
+      race_list_page.course_name = course_name
+    end
+    race_list_page.content = content
+
+    race_list_page
+  end
+
 end

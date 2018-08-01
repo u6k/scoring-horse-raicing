7a30256c8dcd95308238c3c6709f2691ab9e22ec
u6k <u6k.apps@gmail.com>
2018-08-01 12:02:56 +0900

    出走時刻要素を追加した

--- db/migrate/20180801023110_create_result_pages.rb
+++ db/migrate/20180801023110_create_result_pages.rb
@@ -3,6 +3,7 @@ class CreateResultPages < ActiveRecord::Migration[5.2]
     create_table :result_pages do |t|
       t.string :url
       t.integer :race_number
+      t.datetime :start_datetime
       t.string :race_name
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_080229) do
+ActiveRecord::Schema.define(version: 2018_08_01_023110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2018_07_31_080229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["schedule_page_id"], name: "index_race_list_pages_on_schedule_page_id"
+  end
+
+  create_table "result_pages", force: :cascade do |t|
+    t.string "url"
+    t.integer "race_number"
+    t.datetime "start_datetime"
+    t.string "race_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "schedule_pages", force: :cascade do |t|

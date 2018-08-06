c18415978721f8c3d17fdeba69c8bb5c6859e09c
u6k <u6k.apps@gmail.com>
2018-08-06 14:24:08 +0900

    modify job test, and impl job

--- test/integration/tasks/crawl_test.rb
+++ test/integration/tasks/crawl_test.rb
@@ -1,38 +1,66 @@
 require 'test_helper'
-require 'rake'
 
 class CrawlTest < ActionDispatch::IntegrationTest
 
-#  def setup
-#    Rails.application.load_tasks
-#
-#    @bucket = NetModule.get_s3_bucket
-#    @bucket.objects.batch_delete!
-#  end
-#
-#  def teardown
-#    Rake::Task["crawl:download_schedule_pages"].clear
-#    Rake::Task["crawl:download_race_list_pages"].clear
-#  end
-#
-#  test "download schedule page: case 2018-06" do
-#    # precondition
-#    assert_equal 0, SchedulePage.all.length
-#
-#    assert_not @bucket.object("html/201806/schedule.html").exists?
-#
-#    # execute
-#    Rake::Task["crawl:download_schedule_pages"].invoke(2018, 6)
-#
-#    # NOTE: 引数なしで、全期間をダウンロードする
-#    # Rake::Task["crawl:download_schedule_pages"].invoke
-#
-#    # postcondition
-#    assert_equal 1, SchedulePage.all.length
-#
-#    assert @bucket.object("html/201806/schedule.html").exists?
-#  end
-#
+  def setup
+    @bucket = NetModule.get_s3_bucket
+    @bucket.objects.batch_delete!
+  end
+
+  test "download schedule page: case 2018-06" do
+    # execute
+    `rails crawl:download_schedule_pages[2018,6]`
+
+    # NOTE: 引数なしで、全期間をダウンロードする
+    # `rails crawl:download_schedule_pages`
+
+    # check
+    schedule_pages = SchedulePage.find_all
+
+    assert_equal 1, schedule_pages.length
+
+    schedule_page = schedule_pages[0]
+    assert_equal Time.zone.local(2018, 6, 1), schedule_page.date
+  end
+
+  test "download schedule page: case 2018-06, and force download" do
+    # setup
+    schedule_page_html_201806 = File.open("test/fixtures/files/schedule.201806.html").read
+    NetModule.put_s3_object(NetModule.get_s3_bucket, "html/schedule/schedule.201806.html", schedule_page_html_201806)
+
+    assert_equal 1, SchedulePage.find_all.length
+
+    # execute
+    `rails crawl:download_schedule_pages[2018,6,false]`
+
+    # check
+    schedule_pages = SchedulePage.find_all
+
+    assert_equal 1, schedule_pages.length
+
+    schedule_page = schedule_pages[0]
+    assert_equal Time.zone.local(2018, 6, 1), schedule_page.date
+  end
+
+  test "download schedule page: case 2018-06, and missing only" do
+    # setup
+    schedule_page_html_201806 = File.open("test/fixtures/files/schedule.201806.html").read
+    NetModule.put_s3_object(NetModule.get_s3_bucket, "html/schedule/schedule.201806.html", schedule_page_html_201806)
+
+    assert_equal 1, SchedulePage.find_all.length
+
+    # execute
+    `rails crawl:download_schedule_pages[2018,6,true]`
+
+    # check
+    schedule_pages = SchedulePage.find_all
+
+    assert_equal 1, schedule_pages.length
+
+    schedule_page = schedule_pages[0]
+    assert_equal Time.zone.local(2018, 6, 1), schedule_page.date
+  end
+
 #  test "download race list page: case 2018-06" do
 #    # precondition
 #    html = File.open("test/fixtures/files/schedule.201806.html").read

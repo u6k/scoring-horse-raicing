f9a0587286a214da40cae7fd31ce4998c6753f43
u6k <u6k.apps@gmail.com>
2018-08-09 00:09:17 +0900

    レース一覧ページのダウンロードタスクを実装した

--- lib/tasks/crawl.rake
+++ lib/tasks/crawl.rake
@@ -52,26 +52,43 @@ namespace :crawl do
     raise "failed" if task_failed
   end
 
-  task :download_race_list_pages, [:year, :month] => :environment do |task, args|
-    Rails.logger.info "download_race_list_pages: start: year=#{args.year}, month=#{args.month}"
+  task :download_race_list_pages, [:year, :month, :missing_only] => :environment do |task, args|
+    Rails.logger.info "download_race_list_pages: start: year=#{args.year}, month=#{args.month}, missing_only=#{args.missing_only}"
 
+    # setup
     if args.year.nil? || args.month.nil?
-      schedule_pages = SchedulePage.all
+      schedule_pages = SchedulePage.find_all
     else
-      if SchedulePage.find_by_date(args.year, args.month).nil?
-        raise "SchedulePage not found: year=#{args.year}, month=#{args.month}"
-      end
-
-      schedule_pages = [SchedulePage.find_by_date(args.year, args.month)]
+      schedule_pages = [SchedulePage.new(args.year.to_i, args.month.to_i)]
     end
 
-    task_failed = false
+    missing_only = (args.missing_only == "false" ? false : true)
+
+    Rails.logger.info "download_race_list_pages: args: schedule_pages=#{schedule_pages.map{|s|s.date.strftime('%Y%m')}}, missing_only=#{missing_only}"
+
+    # schedule_page download from s3
+    race_list_pages = []
+
     schedule_pages.each.with_index(1) do |schedule_page, index|
-      Rails.logger.info "download_race_list_pages: #{index}/#{schedule_pages.length}: start: date=#{schedule_page.date.strftime('%Y%m')}"
+      Rails.logger.info "download_race_list_pages: schedule_page download from s3: #{index}/#{schedule_pages.length}: date=#{schedule_page.date.strftime('%Y%m')}"
+
+      schedule_page.download_from_s3!
+
+      race_list_pages += schedule_page.race_list_pages
+    end
+
+    # race_list_page download from web
+    task_failed = false
+    race_list_pages.each.with_index(1) do |race_list_page, index|
+      Rails.logger.info "download_race_list_pages: race_list_page download: #{index}/#{race_list_pages.length}: race_id=#{race_list_page.race_id}"
       begin
-        race_list_pages = schedule_page.download_race_list_pages
-        race_list_pages.each { |r| r.save! }
-        Rails.logger.info "download_race_list_pages: #{index}/#{schedule_pages.length}: end"
+        if missing_only && race_list_page.exists?
+          Rails.logger.info "download_race_list_pages: race_list_page download: #{index}/#{race_list_pages.length}: skip"
+        else
+          race_list_page.download_from_web!
+          race_list_page.save!
+          Rails.logger.info "download_race_list_pages: race_list_page download: #{index}/#{race_list_pages.length}: download and saved"
+        end
       rescue => e
         Rails.logger.error build_error_log(e)
         task_failed = true
--- test/integration/tasks/crawl_test.rb
+++ test/integration/tasks/crawl_test.rb
@@ -63,9 +63,7 @@ class CrawlTest < ActionDispatch::IntegrationTest
 
   test "download race list page: case 2018-06(default missing only)" do
     # setup
-    schedule_page_html = File.open("test/fixtures/files/schedule.201806.html").read
-    schedule_page = SchedulePage.new(2018, 6, schedule_page_html)
-    schedule_page.save!
+    `rails crawl:download_schedule_pages[2018,6]`
 
     # execute
     `rails crawl:download_race_list_pages[2018,6]`
@@ -76,7 +74,7 @@ class CrawlTest < ActionDispatch::IntegrationTest
     # check
     race_list_pages = RaceListPage.find_all
 
-    assert_race_list_201806(race_list_pages)
+    assert_race_list_201806
   end
 
   test "download race list page: case 2018-06, and force download" do
@@ -87,7 +85,7 @@ class CrawlTest < ActionDispatch::IntegrationTest
     `rails crawl:download_race_list_pages[2018,6,false]`
 
     # check
-    assert_race_list_201806(race_list_pages)
+    assert_race_list_201806
   end
 
   test "download race list page: case 2018-06, and missing only" do
@@ -98,192 +96,170 @@ class CrawlTest < ActionDispatch::IntegrationTest
     `rails crawl:download_race_list_pages[2018,6,true]`
 
     # check
-    assert_race_list_201806(race_list_pages)
+    assert_race_list_201806
   end
 
   def assert_race_list_201806
     race_list_pages = RaceListPage.find_all
+    race_list_pages.each { |r| r.download_from_s3! }
 
     assert_equal 23, race_list_pages.length
 
-    race_list_page = race_list_pages[0]
-    assert_equal "18050301", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050301" }
     assert_equal Time.zone.local(2018, 6, 2), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[1]
-    assert_equal "18090301", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090301" }
     assert_equal Time.zone.local(2018, 6, 2), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[2]
-    assert_equal "18050302", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050302" }
     assert_equal Time.zone.local(2018, 6, 3), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[3]
-    assert_equal "18090302", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090302" }
     assert_equal Time.zone.local(2018, 6, 3), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[4]
-    assert_equal "18050303", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050303" }
     assert_equal Time.zone.local(2018, 6, 9), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[5]
-    assert_equal "18090303", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090303" }
     assert_equal Time.zone.local(2018, 6, 9), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[6]
-    assert_equal "18050304", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050304" }
     assert_equal Time.zone.local(2018, 6, 10), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[7]
-    assert_equal "18090304", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090304" }
     assert_equal Time.zone.local(2018, 6, 10), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[8]
-    assert_equal "18020101", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18020101" }
     assert_equal Time.zone.local(2018, 6, 16), race_list_page.date
     assert_equal "函館", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[9]
-    assert_equal "18050305", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050305" }
     assert_equal Time.zone.local(2018, 6, 16), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[10]
-    assert_equal "18090305", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090305" }
     assert_equal Time.zone.local(2018, 6, 16), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[11]
-    assert_equal "18020102", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18020102" }
     assert_equal Time.zone.local(2018, 6, 17), race_list_page.date
     assert_equal "函館", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[12]
-    assert_equal "18050306", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050306" }
     assert_equal Time.zone.local(2018, 6, 17), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[13]
-    assert_equal "18090306", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090306" }
     assert_equal Time.zone.local(2018, 6, 17), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[14]
-    assert_equal "18020103", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18020103" }
     assert_equal Time.zone.local(2018, 6, 23), race_list_page.date
     assert_equal "函館", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[15]
-    assert_equal "18050307", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050307" }
     assert_equal Time.zone.local(2018, 6, 23), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[16]
-    assert_equal "18090307", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090307" }
     assert_equal Time.zone.local(2018, 6, 23), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[17]
-    assert_equal "18020104", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18020104" }
     assert_equal Time.zone.local(2018, 6, 24), race_list_page.date
     assert_equal "函館", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[18]
-    assert_equal "18050308", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18050308" }
     assert_equal Time.zone.local(2018, 6, 24), race_list_page.date
     assert_equal "東京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[19]
-    assert_equal "18090308", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18090308" }
     assert_equal Time.zone.local(2018, 6, 24), race_list_page.date
     assert_equal "阪神", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[20]
-    assert_equal "18020105", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18020105" }
     assert_equal Time.zone.local(2018, 6, 30), race_list_page.date
     assert_equal "函館", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[21]
-    assert_equal "18030201", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18030201" }
     assert_equal Time.zone.local(2018, 6, 30), race_list_page.date
     assert_equal "福島", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length
     assert race_list_page.valid?
     assert race_list_page.exists?
 
-    race_list_page = race_list_pages[22]
-    assert_equal "18070301", race_list_page.race_id
+    race_list_page = race_list_pages.find { |r| r.race_id == "18070301" }
     assert_equal Time.zone.local(2018, 6, 30), race_list_page.date
     assert_equal "中京", race_list_page.course_name
     assert_equal 12, race_list_page.result_pages.length

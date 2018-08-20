98494297f8cf8f10c956a7d39e86c16fc34b5914
u6k <u6k.apps@gmail.com>
2018-08-20 11:37:14 +0900

    保存を実装した

--- app/models/concerns/horse_page.rb
+++ app/models/concerns/horse_page.rb
@@ -3,9 +3,31 @@ class HorsePage
 
   attr_reader :horse_id, :horse_name
 
+  def self.find_all
+    horse_pages = NetModule.get_s3_bucket.objects(prefix: "html/horse/horse.").map do |s3_obj|
+      s3_obj.key.match(/horse\.([0-9]+)\.html$/) do |path|
+        HorsePage.new(path[1])
+      end
+    end
+
+    horse_pages.compact
+  end
+
   def initialize(horse_id, content = nil)
     @horse_id = horse_id
     @content = content
+
+    _parse
+  end
+
+  def download_from_web!
+    @content = NetModule.download_with_get(_build_url)
+
+    _parse
+  end
+
+  def download_from_s3!
+    @content = NetModule.get_s3_object(NetModule.get_s3_bucket, _build_s3_path)
 
     _parse
   end
@@ -17,6 +39,14 @@ class HorsePage
 
   def exists?
     NetModule.get_s3_bucket.object(_build_s3_path).exists?
+  end
+
+  def save!
+    if not valid?
+      raise "Invalid"
+    end
+
+    NetModule.put_s3_object(NetModule.get_s3_bucket, _build_s3_path, @content)
   end
 
   private
@@ -31,6 +61,10 @@ class HorsePage
     doc.xpath("//div[@id='dirTitName']/h1").each do |h1|
       @horse_name = h1.text.strip
     end
+  end
+
+  def _build_url
+    "https://keiba.yahoo.co.jp/directory/horse/#{@horse_id}/"
   end
 
   def _build_s3_path

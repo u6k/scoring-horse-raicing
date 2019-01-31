925de9d17b78f35a806f18e03e4425828eace299
u6k <u6k.apps@gmail.com>
2019-01-31 18:28:31 +0900

    Fixed: modify require

--- lib/scoring_horse_racing.rb
+++ lib/scoring_horse_racing.rb
@@ -4,19 +4,7 @@ require "scoring_horse_racing/version"
 
 require "scoring_horse_racing/cli"
 
-require "scoring_horse_racing/rule/entry_page"
-require "scoring_horse_racing/rule/horse_page"
-require "scoring_horse_racing/rule/jockey_page"
-require "scoring_horse_racing/rule/odds_exacta_page"
-require "scoring_horse_racing/rule/odds_quinella_page"
-require "scoring_horse_racing/rule/odds_quinella_place_page"
-require "scoring_horse_racing/rule/odds_trifecta_page"
-require "scoring_horse_racing/rule/odds_trio_page"
-require "scoring_horse_racing/rule/odds_win_page"
-require "scoring_horse_racing/rule/race_list_page"
-require "scoring_horse_racing/rule/result_page"
-require "scoring_horse_racing/rule/schedule_page"
-require "scoring_horse_racing/rule/trainer_page"
+require "scoring_horse_racing/parser/schedule_page"
 
 module ScoringHorseRacing
   class AppLogger

a0ac4e7af9948e7e4844f3329470531c08406158
u6k <u6k.apps@gmail.com>
2019-02-01 19:06:06 +0900

    Changed: Refactor and impl race list page parser

---
+++ lib/scoring_horse_racing/parser/race_list_page.rb
@@ -0,0 +1,86 @@
+require "nokogiri"
+require "crawline"
+
+module ScoringHorseRacing::Parser
+  class RaceListPageParser < Crawline::BaseParser
+    def initialize(url, data)
+      @logger = ScoringHorseRacing::AppLogger.get_logger
+      @logger.debug("RaceListPageParser#initialize: start: url=#{url}, data.size=#{data.size}")
+
+      @url = url
+      @data = data
+
+      _parse
+    end
+
+    def redownload?
+      @logger.debug("RaceListPageParser#redownload?: start: Date.today.prev_month(3)=#{Date.today.prev_month(3)}, @date=#{@date}")
+
+      (Date.today.prev_month(3) < @date)
+    end
+
+    def valid?
+      ((not @related_links.empty?) && (not @race_id.nil?) && (not @date.nil?) && (not @course_name.nil?))
+    end
+
+    def related_links
+      @related_links
+    end
+
+    def parse(context)
+      context["races"] = {
+        @race_id => {
+          "race_id" => @race_id,
+          "date" => @date,
+          "course_name" => @course_name
+        }
+      }
+    end
+
+    private
+
+    def _parse
+      @logger.debug("RaceListPageParser#_parse: start")
+
+      @race_id = @url.match(/^.+?\/race\/list\/([0-9]+)\/$/)[1]
+
+      doc = Nokogiri::HTML.parse(@data, nil, "UTF-8")
+
+      h4 = doc.at_xpath("//div[@id='cornerTit']/h4")
+      if not h4.nil?
+        @logger.debug("RaceListPageParser#_parse: h4=#{h4}")
+
+        h4.text.match(/^([0-9]+)年([0-9]+)月([0-9]+)日/) do |date_parts|
+          @logger.debug("RaceListPageParser#_parse: date_parts=#{date_parts.inspect}")
+
+          @date = Date.new(date_parts[1].to_i, date_parts[2].to_i, date_parts[3].to_i)
+        end
+      end
+
+      a = doc.at_xpath("//li[@id='racePlaceNaviC']/a")
+      if not a.nil?
+        @logger.debug("RaceListPageParser#_parse: a=#{a}")
+
+        @course_name = a.text
+      end
+
+      @related_links = doc.xpath("//table[@class='scheLs']/tbody/tr").map do |tr|
+        @logger.debug("RaceListPageParser#_parse: tr=#{tr}")
+
+        a = tr.at_xpath("td[@class='wsLB']/a")
+        if not a.nil?
+          @logger.debug("RaceListPageParser#_parse: a=#{a}")
+
+          a.attribute("href").value.match(/^\/race\/result\/([0-9]+)\/$/) do |path|
+            @logger.debug("RaceListPageParser#_parse: path=#{path.inspect}")
+
+            URI.join(@url, path[0]).to_s
+          end
+        end
+      end
+
+      @related_links.compact!
+    end
+  end
+end
+

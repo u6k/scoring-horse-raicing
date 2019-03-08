ed281b3fd3dc4e4408a609f649bd16559cc6d236
u6k <u6k.apps@gmail.com>
2019-03-08 17:50:54 +0900

    Changed: impl parse month link at schedule list page

--- spec/parser/schedule_page_spec.rb
+++ spec/parser/schedule_page_spec.rb
@@ -16,6 +16,12 @@ RSpec.describe ScoringHorseRacing::Parser::SchedulePageParser do
 
     @parser_201808 = ScoringHorseRacing::Parser::SchedulePageParser.new(url, data)
 
+    # 1986-01 schedule page parser
+    url = "https://keiba.yahoo.co.jp/schedule/list/1986/?month=01"
+    data = File.open("spec/data/schedule.198601.html").read
+
+    @parser_198601 = ScoringHorseRacing::Parser::SchedulePageParser.new(url, data)
+
     # error page parser
     url = "https://keiba.yahoo.co.jp/schedule/list/1900/?month=01"
     data = File.open("spec/data/schedule.190001.html").read
@@ -61,8 +67,20 @@ RSpec.describe ScoringHorseRacing::Parser::SchedulePageParser do
 
   describe "#related_links" do
     context "2018-06 schedule page" do
-      it "is race list pages" do
+      it "is race list pages, and schedule list pages" do
         expect(@parser.related_links).to contain_exactly(
+          "https://keiba.yahoo.co.jp/schedule/list/2017/?month=12",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=1",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=2",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=3",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=4",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=5",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=7",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=8",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=9",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=10",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=11",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=12",
           "https://keiba.yahoo.co.jp/race/list/18050301/",
           "https://keiba.yahoo.co.jp/race/list/18090301/",
           "https://keiba.yahoo.co.jp/race/list/18050302/",
@@ -90,14 +108,66 @@ RSpec.describe ScoringHorseRacing::Parser::SchedulePageParser do
     end
 
     context "2018-08 schedule page (case link is incomplete)" do
-      it "is race list pages" do
+      it "is race list pages, and schedule list pages" do
         expect(@parser_201808.related_links).to contain_exactly(
+          "https://keiba.yahoo.co.jp/schedule/list/2017/?month=12",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=1",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=2",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=3",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=4",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=5",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=6",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=7",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=9",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=10",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=11",
+          "https://keiba.yahoo.co.jp/schedule/list/2018/?month=12",
           "https://keiba.yahoo.co.jp/race/list/18010103/",
           "https://keiba.yahoo.co.jp/race/list/18040203/",
           "https://keiba.yahoo.co.jp/race/list/18100203/",
           "https://keiba.yahoo.co.jp/race/list/18010104/",
           "https://keiba.yahoo.co.jp/race/list/18040204/",
           "https://keiba.yahoo.co.jp/race/list/18100204/")
+      end
+    end
+
+    context "1986-01 schedule page" do
+      it "is race list pages, and schedule list pages" do
+        expect(@parser_198601.related_links).to contain_exactly(
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=2",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=3",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=4",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=5",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=6",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=7",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=8",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=9",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=10",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=11",
+          "https://keiba.yahoo.co.jp/schedule/list/1986/?month=12",
+          "https://keiba.yahoo.co.jp/schedule/list/1987/?month=1",
+          "https://keiba.yahoo.co.jp/race/list/86060101/",
+          "https://keiba.yahoo.co.jp/race/list/86080101/",
+          "https://keiba.yahoo.co.jp/race/list/86060102/",
+          "https://keiba.yahoo.co.jp/race/list/86080102/",
+          "https://keiba.yahoo.co.jp/race/list/86060103/",
+          "https://keiba.yahoo.co.jp/race/list/86080103/",
+          "https://keiba.yahoo.co.jp/race/list/86060104/",
+          "https://keiba.yahoo.co.jp/race/list/86080104/",
+          "https://keiba.yahoo.co.jp/race/list/86060105/",
+          "https://keiba.yahoo.co.jp/race/list/86080105/",
+          "https://keiba.yahoo.co.jp/race/list/86060106/",
+          "https://keiba.yahoo.co.jp/race/list/86080106/",
+          "https://keiba.yahoo.co.jp/race/list/86060107/",
+          "https://keiba.yahoo.co.jp/race/list/86080107/",
+          "https://keiba.yahoo.co.jp/race/list/86060108/",
+          "https://keiba.yahoo.co.jp/race/list/86080108/",
+          "https://keiba.yahoo.co.jp/race/list/86050101/",
+          "https://keiba.yahoo.co.jp/race/list/86070101/",
+          "https://keiba.yahoo.co.jp/race/list/86080201/",
+          "https://keiba.yahoo.co.jp/race/list/86050102/",
+          "https://keiba.yahoo.co.jp/race/list/86070102/",
+          "https://keiba.yahoo.co.jp/race/list/86080202/")
       end
     end
   end

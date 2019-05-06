76bf0fef41ff436dc257e28e2d1db8670dbf4948
u6k <u6k.apps@gmail.com>
2019-05-06 17:28:34 +0900

    Fixed: entry page parse

--- lib/investment_horse_racing/crawler/parser/entry_page.rb
+++ lib/investment_horse_racing/crawler/parser/entry_page.rb
@@ -56,17 +56,36 @@ module InvestmentHorseRacing::Crawler::Parser
           horse_number: tr.at_xpath("td[2]").text.strip,
           horse_id: tr.at_xpath("td[3]/a[1]")["href"][/\/directory\/horse\/(\d+)\//, 1],
           horse_name: tr.at_xpath("td[3]/a[1]").text.strip,
-          gender: tr.at_xpath("td[3]/span/text()").text[/^(.+?)(\d+)\/(.+)$/, 1],
-          age: tr.at_xpath("td[3]/span/text()").text[/^(.+?)(\d+)\/(.+)$/, 2].to_i,
-          coat_color: tr.at_xpath("td[3]/span/text()").text[/^(.+?)(\d+)\/(.+)$/, 3].strip,
-          trainer_id: tr.at_xpath("td[3]/span/a")["href"][/\/directory\/trainer\/(\d+)\//, 1],
-          trainer_name: tr.at_xpath("td[3]/span/a").text.strip,
+          trainer_id: nil,
+          trainer_name: nil,
           horse_weight: tr.at_xpath("td[4]/text()").text.to_i,
-          jockey_id: tr.at_xpath("td[5]/a")["href"][/\/directory\/jocky\/(\d+)\//, 1],
-          jockey_name: tr.at_xpath("td[5]/a").text.strip,
+          jockey_id: nil,
+          jockey_name: nil,
           jockey_weight: tr.at_xpath("td[5]/text()").text.strip.to_f,
           father_horse_name: tr.at_xpath("td[6]/text()[1]").text.strip,
           mother_horse_name: tr.at_xpath("td[6]/text()[2]").text.strip)
+
+        tr.xpath("td[3]/span/a").each do |a|
+          entry["trainer_id"] = a["href"][/\/directory\/trainer\/(\d+)\//, 1]
+          entry["trainer_name"] = a.text.strip
+        end
+
+        tr.xpath("td[5]/a").each do |a|
+          entry["jockey_id"] = a["href"][/\/directory\/jocky\/(\d+)\//, 1]
+          entry["jockey_name"] = a.text.strip
+        end
+
+        if tr.at_xpath("td[3]/span/text()").text.match(/^(.+?)(\d+)\/(.+)$/)
+          entry[:gender] = tr.at_xpath("td[3]/span/text()").text[/^(.+?)(\d+)\/(.+)$/, 1]
+          entry[:age] = tr.at_xpath("td[3]/span/text()").text[/^(.+?)(\d+)\/(.+)$/, 2].to_i
+          entry[:coat_color] = tr.at_xpath("td[3]/span/text()").text[/^(.+?)(\d+)\/(.+)$/, 3].strip
+        else
+          entry[:gender] = nil
+          entry[:age] = nil
+          entry[:coat_color] = nil
+        end
+
+        entry
       end
     end
   end

421ea2ffe18bcdea766e95fecc5efb8a2762f349
u6k <u6k.apps@gmail.com>
2020-01-10 19:52:25 +0900

    Changed: schedule list spider

--- investment_horse_racing_crawler/investment_horse_racing_crawler/spiders/schedule_list_spider.py
+++ investment_horse_racing_crawler/investment_horse_racing_crawler/spiders/schedule_list_spider.py
@@ -4,9 +4,9 @@ import scrapy
 class ScheduleListSpider(scrapy.Spider):
     name = "schedule_list"
 
-    def start_requests(self):
-        url = 'https://keiba.yahoo.co.jp/schedule/list/'
-        yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'https://keiba.yahoo.co.jp/schedule/list/',
+    ]
 
     def parse(self, response):
         for title in response.xpath("//title/text()"):

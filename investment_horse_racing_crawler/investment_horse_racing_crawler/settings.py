2339b06ad795efa9acdf30052240f2e8fec9e573
u6k <u6k.apps@gmail.com>
2020-01-10 19:52:15 +0900

    Changed: scrapy settings

--- investment_horse_racing_crawler/investment_horse_racing_crawler/settings.py
+++ investment_horse_racing_crawler/investment_horse_racing_crawler/settings.py
@@ -7,3 +7,10 @@ NEWSPIDER_MODULE = 'investment_horse_racing_crawler.spiders'
 
 
 ROBOTSTXT_OBEY = True
+
+DOWNLOAD_DELAY = 3
+
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 1
+
+USER_AGENT = "horse_racing_crawler/1.0 (+https://github.com/u6k/investment-horse-racing-crawler)"

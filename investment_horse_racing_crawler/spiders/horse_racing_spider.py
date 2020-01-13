2e893360b47dd899fbdf8f9518c524f61d40fc1f
u6k <u6k.apps@gmail.com>
2020-01-13 19:48:56 +0900

    Added: parse

--- investment_horse_racing_crawler/spiders/horse_racing_spider.py
+++ investment_horse_racing_crawler/spiders/horse_racing_spider.py
@@ -14,35 +14,67 @@ class HorseRacingSpider(scrapy.Spider):
     def parse(self, response):
         self.logger.debug("#parse: start: url=%s" % response.url)
 
-        for a in response.xpath("//div[@class='scheHeadNaviR fntSS']/a"):
-            self.logger.debug("#parse: other schedule list page: href=%s" % a.xpath("@href").get())
-            yield response.follow(a, callback=self.parse)
+        for a in response.xpath("//a"):
+            href = a.xpath("@href").get()
 
-        for a in response.xpath("//table[@class='scheLs mgnBS']/tbody/tr/td[1][@rowspan='2']/a"):
-            self.logger.debug("#parse: race list page: href=%s" % a.xpath("@href").get())
-            yield response.follow(a, callback=self.parse_race_list)
+            if href.startswith("/schedule/list/"):
+                self.logger.debug("#parse: other schedule list page: href=%s" % href)
+                yield response.follow(a, callback=self.parse)
+
+            if href.startswith("/race/list/"):
+                self.logger.debug("#parse: race list page: href=%s" % href)
+                yield response.follow(a, callback=self.parse_race_list)
 
     def parse_race_list(self, response):
         self.logger.debug("#parse_race_list: start: url=%s" % response.url)
 
-        for a in response.xpath("//table[@class='scheLs']/tbody/tr/td[@class='wsLB']/a"):
-            self.logger.debug("#parse_race_list: race result page: href=%s" % a.xpath("@href").get())
-            yield response.follow(a, callback=self.parse_race_result)
+        for a in response.xpath("//a"):
+            href = a.xpath("@href").get()
+
+            if href.startswith("/race/result/"):
+                self.logger.debug("#parse_race_list: race result page: href=%s" % href)
+                yield response.follow(a, callback=self.parse_race_result)
 
     def parse_race_result(self, response):
         self.logger.debug("#parse_race_result: start: url=%s" % response.url)
 
         for a in response.xpath("//a"):
-            if a.xpath("@href").get().startswith("/race/denma/"):
-                self.logger.debug("#parse_race_result: race denma page: href=%s" % a.xpath("@href").get())
+            href = a.xpath("@href").get()
+
+            if href.startswith("/race/denma/"):
+                self.logger.debug("#parse_race_result: race denma page: href=%s" % href)
                 yield response.follow(a, callback=self.parse_race_denma)
 
-            if a.xpath("@href").get().startswith("/odds/tfw/"):
-                self.logger.debug("#parse_race_result: odds page: href=%s" % a.xpath("@href").get())
+            if href.startswith("/odds/tfw/"):
+                self.logger.debug("#parse_race_result: odds page: href=%s" % href)
                 yield response.follow(a, callback=self.parse_odds)
 
     def parse_race_denma(self, response):
         self.logger.debug("#parse_race_denma: start: url=%s" % response.url)
+
+        for a in response.xpath("//a"):
+            href = a.xpath("@href").get()
+
+            if href.startswith("/directory/horse/"):
+                self.logger.debug("#parse_race_denma: horse page: href=%s" % href)
+                yield response.follow(a, callback=self.parse_horse)
+
+            if href.startswith("/directory/trainer/"):
+                self.logger.debug("#parse_race_denma: trainer page: href=%s" % href)
+                yield response.follow(a, callback=self.parse_trainer)
+
+            if href.startswith("/directory/jocky/"):
+                self.logger.debug("#parse_race_denma: jockey page: href=%s" % href)
+                yield response.follow(a, callback=self.parse_jockey)
+
+    def parse_horse(self, response):
+        self.logger.debug("#parse_horse: start: url=%s" % response.url)
+
+    def parse_trainer(self, response):
+        self.logger.debug("#parse_trainer: start: url=%s" % response.url)
+
+    def parse_jockey(self, response):
+        self.logger.debug("#parse_jockey: start: url=%s" % response.url)
 
     def parse_odds(self, response):
         self.logger.debug("#parse_odds: start: url=%s" % response.url)

9c3c88f7da6b2e1c4d53fb0fcbc31019515bad84
u6k <u6k.apps@gmail.com>
2020-01-14 09:26:47 +0900

    Added: spiders contracts

--- investment_horse_racing_crawler/spiders/horse_racing_spider.py
+++ investment_horse_racing_crawler/spiders/horse_racing_spider.py
@@ -9,6 +9,12 @@ class HorseRacingSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        """ Parse schedule list page.
+
+        @url https://keiba.yahoo.co.jp/schedule/list/2019/?month=12
+        @returns items 0
+        @returns requests 1
+        """
         self.logger.debug("#parse: start: url=%s" % response.url)
 
         for a in response.xpath("//a"):
@@ -23,6 +29,12 @@ class HorseRacingSpider(scrapy.Spider):
                 yield response.follow(a, callback=self.parse_race_list)
 
     def parse_race_list(self, response):
+        """ Parse race list page.
+
+        @url https://keiba.yahoo.co.jp/race/list/19060502/
+        @returns items 0
+        @returns requests 1
+        """
         self.logger.debug("#parse_race_list: start: url=%s" % response.url)
 
         for a in response.xpath("//a"):
@@ -33,6 +45,12 @@ class HorseRacingSpider(scrapy.Spider):
                 yield response.follow(a, callback=self.parse_race_result)
 
     def parse_race_result(self, response):
+        """ Parse race result page.
+
+        @url https://keiba.yahoo.co.jp/race/result/1906050201/
+        @returns items 0
+        @returns requests 2 2
+        """
         self.logger.debug("#parse_race_result: start: url=%s" % response.url)
 
         for a in response.xpath("//a"):
@@ -47,6 +65,12 @@ class HorseRacingSpider(scrapy.Spider):
                 yield response.follow(a, callback=self.parse_odds)
 
     def parse_race_denma(self, response):
+        """ Parse denma page.
+
+        @url https://keiba.yahoo.co.jp/race/denma/1906050201/
+        @returns items 0
+        @returns requests 1
+        """
         self.logger.debug("#parse_race_denma: start: url=%s" % response.url)
 
         for a in response.xpath("//a"):
@@ -65,13 +89,37 @@ class HorseRacingSpider(scrapy.Spider):
                 yield response.follow(a, callback=self.parse_jockey)
 
     def parse_horse(self, response):
+        """ Parse horse page.
+
+        @url https://keiba.yahoo.co.jp/directory/horse/2017101602/
+        @returns items 0
+        @returns requests 0 0
+        """
         self.logger.debug("#parse_horse: start: url=%s" % response.url)
 
     def parse_trainer(self, response):
+        """ Parse trainer page.
+
+        @url https://keiba.yahoo.co.jp/directory/trainer/01012/
+        @returns items 0
+        @returns requests 0 0
+        """
         self.logger.debug("#parse_trainer: start: url=%s" % response.url)
 
     def parse_jockey(self, response):
+        """ Parse jockey page.
+
+        @url https://keiba.yahoo.co.jp/directory/jocky/01167/
+        @returns items 0
+        @returns requests 0 0
+        """
         self.logger.debug("#parse_jockey: start: url=%s" % response.url)
 
     def parse_odds(self, response):
+        """ Parse odds page.
+
+        @url https://keiba.yahoo.co.jp/odds/tfw/1906050201/
+        @returns items 0
+        @returns requests 0 0
+        """
         self.logger.debug("#parse_odds: start: url=%s" % response.url)
